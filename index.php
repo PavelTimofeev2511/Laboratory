08df33eb2d374f85692a075c0c728bff465547d8
PavelTimofeev2511 <paveltimofeev183@gmail.com>
2021-04-06 16:25:25 +0300

    v5.2

--- index.php
+++ index.php
@@ -8,26 +8,26 @@ $answer = new timofeev\QuadraticEquation();
 
 
 try {
-    timofeev\MyLog::log("Version: " . trim(file_get_contents("version")) . "\n");
+    MyLog::Instance()::log("Version: " . trim(file_get_contents("version")) . "\n");
     $a = readline("a= \n\r");
     $b = readline("b= \n\r");
     $c = readline("c= \n\r");
 
     $equation = $a."x2+(".$b.")x+(".$c.")=0";
-    MyLog::log("Equation is $equation");
+    MyLog::Instance()::log("Equation is $equation");
     if ($a == 0) {
-        MyLog::Instance()->log("Линейное уравнение");
+        MyLog::Instance()::log("Линейное уравнение");
     } else {
-        Mylog::Instance()->log("Квадратное уравнение");
+        MyLog::Instance()::log("Квадратное уравнение");
     }
 
     $result = $answer->solve($a, $b, $c);
     $str = implode("", $result);
     $mes = "Корни уравнения.";
-    timofeev\Mylog::log("\n\r". $mes . "\n\r". $str);
+    MyLog::Instance()::log("\n\r". $mes . "\n\r". $str);
 
 } catch (Exception $err) {
-    timofeev\Mylog::log($err);
+    MyLog::Instance()::log($err);
 }
 
-timofeev\Mylog::write();
+MyLog::Instance()::write();
